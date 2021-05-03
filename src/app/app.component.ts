37bcd4c5c97e1e3bb0537d406a74f16772c4e06c
Ian White <whitie1191@googlemail.com>
2021-05-03 06:45:53 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ctt-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'CTTpwa';
+}
